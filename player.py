9eaaeb18eb9a889c8e82be4fd7d0facfc028b2ee
yetanotherape <yetanotherape@gmail.com>
2016-04-09 12:34:51 +0500

    1

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return 0
+        return game_state['small_blind']*8
 
     def showdown(self, game_state):
         pass
